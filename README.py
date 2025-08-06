8ed165a0ffa4dc02593c2245954a7900dfbd965c
Amandeep Kaur <dhiman601.ak@gmail.com>
2025-08-06 21:08:52 +0530

    Add files via upload

---
+++ README.py
@@ -0,0 +1,14 @@
+x=int(input("Enter vlaue1:"))
+y=int(input("Enter value2:"))
+sum=x+y
+diff=x-y
+mult=x*y
+div=x/y
+mod=x%y
+print("Number are:",x,y)
+print("sum=" , sum)
+print("difference=", diff)
+print("Product=",mult)
+print("Division=",div)
+print("Modulus=",mod)
+
